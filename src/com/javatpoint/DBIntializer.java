c25412fdfa67dbbf661e6cdc5f3feed468f03734
bharti.rawat <bharti.rawat@dogmasystems.com>
2021-11-28 17:56:47 +0530

    first

---
+++ src/com/javatpoint/DBIntializer.java
@@ -0,0 +1,8 @@
+package com.javatpoint;
+
+public interface DBIntializer {
+String DRIVER="com.mysql.jdbc.Driver";
+String CON_STRING="jdbc:mysql://localhost:3306/mymail";
+String USERNAME="root";
+String PASSWORD="root";
+}
